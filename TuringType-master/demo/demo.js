28ee08cd70b1fe9f5f6fabcf71db5e6560db4f6a
jukvii <jw@sci.fi>
2019-05-23 22:12:07 +0300

    stuff

--- TuringType-master/demo/demo.js
+++ TuringType-master/demo/demo.js
@@ -1,9 +1,38 @@
 var corpus1 =
-"Hello.\n\
-I am TuringType.\n\
-I am a naïve simulation of a human typing. I was written in a few minutes (and a few lines of CoffeeScript), so I'm not meant to be very accurate. I'm just a parlor trick you can use in your own projects.\n\
-Notice that I occasionally make mistakes. Please excuse me, and note that you can easily change my typing accuracy level and speed.\n\
-Watch this...";
+`
+
+
+package com.wapice.lof.theapplicatoin;\n\
+\n\
+import javax.jms.JMSException;\n\
+\n\
+import org.apache.activemq.command.ActiveMQTextMessage;\n\
+import org.springframework.jms.annotation.JmsListener;\n\
+import org.springframework.stereotype.Component;\n\
+\n\
+@Component\n\
+public class TempReceiver {\n\
+\n\
+	@JmsListener(destination = MainConfiguration.MESSAGE_QUEUE_NAME, containerFactory = MainConfiguration.JMS_LISTENER_FACTORY)\n\
+	public void receiveMessage(ActiveMQTextMessage email) {\n\
+		System.out.println("Received <" + email + ">");\n\
+\n\
+		try {\n\
+			if (email.getText().contains("do not ack")) {\n\
+				email.onMessageRolledBack();\n\
+				throw new RuntimeException();\n\
+			} else {\n\
+				email.acknowledge();\n\
+			}\n\
+		} catch (JMSException e) {\n\
+			// TODO Auto-generated catch block\n\
+			e.printStackTrace();\n\
+		}\n\
+	}\n\
+}\n\
+
+
+`;
 
 var corpus2 =
 "Use me like this:\n\
@@ -16,26 +45,6 @@ new TuringType(domElement, 'Terrible but fast typist.', { accuracy: 0.3, interva
 \n\
 Enjoy. Click the Github link in the upper right to see my code.";
 
-var cb1 = function(){
-    setTimeout(function(){
-        el.value = '';
-        new TuringType(el, 'You can set me to be wildly inaccurate.', { accuracy: .5, callback: cb2 });
-    }, 2000);
-}
-
-var cb2 = function(){
-    setTimeout(function(){
-        el.value = '';
-        new TuringType(el, 'Or maybe wildly inaccurate and very fast for an interesting effect.', { accuracy: .3, interval: 15, callback: cb3 });
-    }, 2000);
-}
-
-var cb3 = function(){
-    setTimeout(function(){
-        el.value = '';
-        new TuringType(el, corpus2);
-    }, 2000);
-}
 
 var el = document.getElementById('test');
-new TuringType(el, corpus1, { callback: cb1 });
+new TuringType(el, corpus1, { interval: 5, accuracy: .6, callback: null });
