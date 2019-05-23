003ebe228ffbb03b7d86cdfffce927b0d6c785ca
Pate Blomqvist <pate.blomqvist@wapice.com>
2019-05-23 18:15:55 +0300

    Add Eureka suppotr and simple test REST to theapplication

--- code/backend/theapplicatoin/src/main/java/com/wapice/lof/theapplicatoin/TheapplicationApplication.java
+++ code/backend/theapplicatoin/src/main/java/com/wapice/lof/theapplicatoin/TheapplicationApplication.java
@@ -1,18 +1,33 @@
 package com.wapice.lof.theapplicatoin;
 
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.netflix.discovery.EurekaClient;
 
 @SpringBootApplication
-public class TheapplicationApplication implements InitializingBean {
+@RestController
+@EnableWebMvc
+public class TheapplicationApplication implements InitializingBean, GreetingController {
 
 	@Value("${someproperty}")
 	private String value;
 
 	@Value("${onlydefaultproperty}")
 	private String onlydefaultproperty;
+	
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+	
+    @Value("${spring.application.name}")
+    private String appName;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TheapplicationApplication.class, args);
@@ -23,4 +38,9 @@ public class TheapplicationApplication implements InitializingBean {
 		System.out.println(value);
 		System.out.println(onlydefaultproperty);
 	}
+
+    @Override
+    public String greeting() {
+        return String.format("Hello from '%s'!", eurekaClient.getApplication(appName).getName());
+    }
 }
